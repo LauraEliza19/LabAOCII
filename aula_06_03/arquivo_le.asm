fd8fe50eb511531bb53356686f0fa5e4fbe1ccd3
Laura Palagano <69463821+LauraEliza19@users.noreply.github.com>
2025-03-06 11:51:34 -0300

    aloca dinamicamente

--- aula_06_03/arquivo_le.asm
+++ aula_06_03/arquivo_le.asm
@@ -3,7 +3,7 @@
 # indicado o caminho completo 
 #
 .data 
-arquivo: .asciiz "C:\\Users\\1396264\\Downloads\\arquivo_leitura.txt"  # nome do arquivo
+arquivo: .asciiz "C:\\Users\\1396264\\Documents\\GitHub\\LabAOCII\\aula_06_03\\arquivo_leitura.txt"  # nome do arquivo
 buffer: .space 1024
 
 .text
@@ -18,27 +18,41 @@ buffer: .space 1024
 	move $s0, $v0      # salva o descritor de arquivo 
 
 #l� do arquivo
-	li   $v0, 14       # chamada de sistema para ler arquivo
-	move $a0, $s0      # descritor do arquivo 
-	la   $a1, buffer   # endere�o do buffer para receber a leitura
-	move $s1, $a1      # salva ponteiro para buffer em $s1
-	li   $a2, 1024     # n�mero m�ximo de caracters a serem lidos
+	
+	# la   $a1, buffer   # endere�o do buffer para receber a leitura
+	# move $s1, $a1      # salva ponteiro para buffer em $s1
+	
+Loop:
+	li $v0, 9
+	li $a0, 1
+	syscall
+	move $a1, $v0
+	
+	# move $a0, $s0      # descritor do arquivo 
+	
+	li   $v0, 14       # chamada de sistema para ler arquivo	
+	li   $a2, 1   # n�mero m�ximo de caracters a serem lidos
 	syscall            # executa leitura do arquivo!
+	
+	beq $v0, 0, fim_arquivo
+	# addi $a1, $a1, 1	
+	j Loop
 
-# Fecha o arquivo 
+# Fecha o arquiv
+fim_arquivo:
 	li   $v0, 16       # chamada de sistema para fechar arquivo
 	move $a0, $s0      # descritor do arquivo a ser fechado
 	syscall            # fecha arquivo!
 
 # imprime conte�do do buffer    
-        add  $t0, $zero, $zero   # i = 0
-    	add  $t1, $s1, $t0       # inicia $t1 com ponteiro $s1
-    L1: lb   $s3, 0($t1) 
-   	beq  $t0, 100, out     # se i = 10 deixar o la�o  
-        jal  imprime_caracter_em_s3  
-        addi $t0, $t0, 1         # i = i + 1 
-        addi $t1, $t1, 1         # incr endere�o  
-        j    L1                  # volta pro come�o do loop
+		add  $t0, $zero, $zero   # i = 0
+    		add  $t1, $s1, $t0       # inicia $t1 com ponteiro $s1
+    	L1: lb   $s3, 0($t1) 
+   		beq  $t0, 100, out     # se i = 10 deixar o la�o  
+        	jal  imprime_caracter_em_s3  
+        	addi $t0, $t0, 1         # i = i + 1 
+        	addi $t1, $t1, 1         # incr endere�o  
+        	j    L1                  # volta pro come�o do loop
     out: 
         
 feito:  li $v0, 10
